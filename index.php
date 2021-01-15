6ce4d64b5d06e6ad5451d830110544cfba3100c0
anupong <annpong2543@gmail.com>
2021-01-15 14:39:14 +0700

    edit index

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 echo "My Hompage";
 ?>
-<a href = "http://">page1</a>
-<a href = "http://">page2</a>
-<a href = "http://">page3</a>
+<a href = "page1.php">page1</a>
+<a href = "page2.php">page2</a>
+<a href = "page3.php">page3</a>
+<a href = "page4.php">page4</a>
+<a href = "page5.php">page5</a>
